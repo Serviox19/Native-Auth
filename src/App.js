7d56a310cde559c4d031b3a39c8358ad4e235017
Servio Mora <servio.mora19@gmail.com>
2017-06-06 12:17:27 -0400

    initialized firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 
-const App = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>
-        Welcome to This App
-      </Text>
-      <Text style={styles.instructions}>
-        To get started, edit index.android.js
-      </Text>
-    </View>
-  );
+class App extends Component {
+  state = { loggedIn: null };
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyAWW3I_Nvpw5ffInOZr4hWWTWBg-utSUNI",
+      authDomain: "native-auth-da159.firebaseapp.com",
+      databaseURL: "https://native-auth-da159.firebaseio.com",
+      projectId: "native-auth-da159",
+      storageBucket: "native-auth-da159.appspot.com",
+      messagingSenderId: "536115688324"
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to This App
+        </Text>
+      </View>
+    );
+  }
 }
 
 const styles = {
   container: {
     flex: 1,
-    justifyContent: 'center',
+    marginTop: 25,
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
